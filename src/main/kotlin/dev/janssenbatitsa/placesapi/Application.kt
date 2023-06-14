6a3dd6f4131849388f598ea5ce7e32a3ea14d88b
Janssen Batista <batistajanssen.dev@gmail.com>
2023-06-14 17:11:34 -0300

    add openapi configuration

--- src/main/kotlin/dev/janssenbatitsa/placesapi/Application.kt
+++ src/main/kotlin/dev/janssenbatitsa/placesapi/Application.kt
@@ -1,11 +1,18 @@
 package dev.janssenbatitsa.placesapi
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PlacesApiApplication
+@OpenAPIDefinition(
+        info = Info(title = "Places API", version = "1.0.0",
+                contact = Contact(name = "Janssen Batista", email = "batistajanssen.dev@gmail.com")
+        ))
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<PlacesApiApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/main/kotlin/dev/janssenbatitsa/placesapi/controllers/PlaceController.kt
+++ src/main/kotlin/dev/janssenbatitsa/placesapi/controllers/PlaceController.kt
@@ -1,8 +1,11 @@
 package dev.janssenbatitsa.placesapi.controllers
 
-import dev.janssenbatitsa.placesapi.services.PlaceService
 import dev.janssenbatitsa.placesapi.models.Place
 import dev.janssenbatitsa.placesapi.models.dtos.PlaceRequestDTO
+import dev.janssenbatitsa.placesapi.services.PlaceService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -17,35 +20,62 @@ import java.util.*
 @RequestMapping("/places")
 class PlaceController(private val placeService: PlaceService) {
 
-    @PostMapping
+    @PostMapping(consumes = ["application/json"], produces = ["application/json"])
+    @Operation(summary = "Create a place")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "201", description = "place created"),
+                ApiResponse(responseCode = "409", description = "place with the same name already exists")
+            ]
+    )
     fun createPlace(@RequestBody @Valid placeRequestDTO: PlaceRequestDTO): ResponseEntity<Place> {
         val createdPlace = placeService.save(placeRequestDTO)
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace)
     }
 
-    @PutMapping("/{placeId}")
+    @PutMapping("/{placeId}", consumes = ["application/json"], produces = ["application/json"])
+    @Operation(summary = "Update a place")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "place updated"),
+                ApiResponse(responseCode = "404", description = "place with the id specified not found")
+            ]
+    )
     fun updatePlace(
-        @PathVariable placeId: UUID,
-        @RequestBody @Valid placeRequestDTO: PlaceRequestDTO
+            @PathVariable placeId: UUID,
+            @RequestBody @Valid placeRequestDTO: PlaceRequestDTO
     ): ResponseEntity<Place> {
         val updatedPlace = placeService.update(placeId, placeRequestDTO)
         return ResponseEntity.status(HttpStatus.OK).body(updatedPlace)
     }
 
-    @GetMapping("/{placeId}")
+    @GetMapping("/{placeId}", produces = ["application/json"])
+    @Operation(summary = "Get a place by id")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "place found"),
+                ApiResponse(responseCode = "404", description = "place with the id specified not found")
+            ]
+    )
     fun getPlaceById(@PathVariable placeId: UUID): ResponseEntity<Place> {
         return ResponseEntity.ok(placeService.findPlaceById(placeId))
     }
 
-    @GetMapping
+    @GetMapping(produces = ["application/json"])
+    @Operation(summary = "Get all places or places filtered by name")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "200", description = "places found"),
+            ]
+    )
     fun getAllPlaces(
-        @RequestParam(required = false) name: String?,
-        @PageableDefault(
-            page = 0,
-            size = 10,
-            sort = ["name"],
-            direction = Sort.Direction.ASC
-        ) pageable: Pageable
+            @RequestParam(required = false) name: String?,
+            @PageableDefault(
+                    page = 0,
+                    size = 10,
+                    sort = ["name"],
+                    direction = Sort.Direction.ASC
+            ) pageable: Pageable
     ): ResponseEntity<Page<Place>> {
         return if (name != null) {
             ResponseEntity.ok(placeService.findAllByName(name, pageable))
@@ -55,6 +85,13 @@ class PlaceController(private val placeService: PlaceService) {
     }
 
     @DeleteMapping("/{placeId}")
+    @Operation(summary = "Delete a place by id")
+    @ApiResponses(
+            value = [
+                ApiResponse(responseCode = "204", description = "place deleted"),
+                ApiResponse(responseCode = "404", description = "place with the id specified not found")
+            ]
+    )
     fun deletePlace(@PathVariable placeId: UUID): ResponseEntity<Any> {
         placeService.deleteById(placeId)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
