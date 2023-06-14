f12156f244781f18358ccdcaf5bb96fd0cf2f6a4
Janssen Batista <batistajanssen.dev@gmail.com>
2023-06-14 09:57:42 -0300

    update integration tests to place controller

--- src/test/kotlin/dev/janssenbatitsa/placesapi/controllers/PlaceControllerIntegrationTest.kt
+++ src/test/kotlin/dev/janssenbatitsa/placesapi/controllers/PlaceControllerIntegrationTest.kt
@@ -1,12 +1,11 @@
 package dev.janssenbatitsa.placesapi.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import dev.janssenbatitsa.placesapi.PlaceService
 import dev.janssenbatitsa.placesapi.models.dtos.PlaceRequestDTO
 import dev.janssenbatitsa.placesapi.repositories.PlaceRepository
+import dev.janssenbatitsa.placesapi.services.PlaceService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -39,16 +38,11 @@ class PlaceControllerIntegrationTest {
     @Autowired
     private lateinit var mapper: ObjectMapper
 
-    private lateinit var placeRequestDTO: PlaceRequestDTO
-
-    @BeforeEach
-    fun setup() {
-        placeRequestDTO = PlaceRequestDTO(
+    private val placeRequestDTO = PlaceRequestDTO(
             name = "Place Name",
             city = "Place City",
             state = "Place State"
-        )
-    }
+    )
 
     @AfterEach
     fun tearDown() {
@@ -61,18 +55,18 @@ class PlaceControllerIntegrationTest {
     fun `should be able to create a place and return status code 201`() {
         val dtoAsString = mapper.writeValueAsString(placeRequestDTO)
         mockMvc.perform(
-            post(BASE_REQUEST_URI)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(dtoAsString)
+                post(BASE_REQUEST_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoAsString)
         )
-            .andExpect(MockMvcResultMatchers.status().isCreated)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(placeRequestDTO.name))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("place-name"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(placeRequestDTO.city))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(placeRequestDTO.state))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.status().isCreated)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(placeRequestDTO.name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("place-name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(placeRequestDTO.city))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(placeRequestDTO.state))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
     }
 
     @Test
@@ -80,10 +74,10 @@ class PlaceControllerIntegrationTest {
         val createdPlace = placeRepository.save(placeRequestDTO.toPlace())
         val dtoAsString = mapper.writeValueAsString(placeRequestDTO)
         val response =
-            mockMvc.perform(post(BASE_REQUEST_URI).contentType(MediaType.APPLICATION_JSON).content(dtoAsString))
-                .andExpect(MockMvcResultMatchers.status().isConflict).andReturn()
+                mockMvc.perform(post(BASE_REQUEST_URI).contentType(MediaType.APPLICATION_JSON).content(dtoAsString))
+                        .andExpect(MockMvcResultMatchers.status().isConflict).andReturn()
         assertThat(response.response.contentAsString)
-            .isEqualTo("Place with name ${createdPlace.name} already exists!")
+                .isEqualTo("Place with name ${createdPlace.name} already exists!")
     }
 
     // Update place
@@ -92,43 +86,43 @@ class PlaceControllerIntegrationTest {
     fun `should be able to update a place and return status code 200`() {
         val createdPlace = placeRepository.save(placeRequestDTO.toPlace())
         val updatePlaceRequestDTO = PlaceRequestDTO(
-            name = "New Place Name",
-            city = "New Place City",
-            state = "New Place State"
+                name = "New Place Name",
+                city = "New Place City",
+                state = "New Place State"
         )
         val dtoAsString = mapper.writeValueAsString(updatePlaceRequestDTO)
         mockMvc.perform(
-            put("$BASE_REQUEST_URI/${createdPlace.id}")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(dtoAsString)
+                put("$BASE_REQUEST_URI/${createdPlace.id}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoAsString)
         )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(createdPlace.id.toString()))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(updatePlaceRequestDTO.name))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("new-place-name"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(updatePlaceRequestDTO.city))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(updatePlaceRequestDTO.state))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(createdPlace.id.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(updatePlaceRequestDTO.name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("new-place-name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(updatePlaceRequestDTO.city))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(updatePlaceRequestDTO.state))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
     }
 
     @Test
     fun `should not be able to update a place with nonexistent id and return status code 404`() {
         val placeId = UUID.randomUUID()
         val updatePlaceRequestDTO = PlaceRequestDTO(
-            name = "New Place Name",
-            city = "New Place City",
-            state = "New Place State"
+                name = "New Place Name",
+                city = "New Place City",
+                state = "New Place State"
         )
         val dtoAsString = mapper.writeValueAsString(updatePlaceRequestDTO)
         val response = mockMvc.perform(
-            put("$BASE_REQUEST_URI/$placeId")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(dtoAsString)
+                put("$BASE_REQUEST_URI/$placeId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoAsString)
         )
-            .andExpect(MockMvcResultMatchers.status().isNotFound)
-            .andReturn()
-        assertThat(response.response.contentAsString).isEqualTo("Place with id $placeId not found")
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+                .andReturn()
+        assertThat(response.response.contentAsString).isEqualTo("Place with id $placeId not found!")
     }
 
     // Find and filter places
@@ -137,14 +131,23 @@ class PlaceControllerIntegrationTest {
     fun `should be able to find a place by id and return status code 200`() {
         val createdPlace = placeRepository.save(placeRequestDTO.toPlace())
         mockMvc.perform(get("$BASE_REQUEST_URI/${createdPlace.id}"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(createdPlace.id.toString()))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(createdPlace.name))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("place-name"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(createdPlace.city))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(createdPlace.state))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(createdPlace.id.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(createdPlace.name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.slug").value("place-name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.city").value(createdPlace.city))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.state").value(createdPlace.state))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdAt").isNotEmpty)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updatedAt").isNotEmpty)
+    }
+
+    @Test
+    fun `should not be able to find a place by id and return status code 404`() {
+        val placeId = UUID.randomUUID()
+        val response = mockMvc.perform(get("$BASE_REQUEST_URI/$placeId"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+                .andReturn()
+        assertThat(response.response.contentAsString).isEqualTo("Place with id $placeId not found!")
     }
 
     @Test
@@ -153,8 +156,8 @@ class PlaceControllerIntegrationTest {
         val placeTwo = PlaceRequestDTO(name = "Name two", city = "City two", state = "State two")
         val places = placeRepository.saveAll(listOf(placeOne.toPlace(), placeTwo.toPlace()))
         mockMvc.perform(get(BASE_REQUEST_URI))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfElements").value(places.size))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfElements").value(places.size))
     }
 
     @Test
@@ -164,8 +167,8 @@ class PlaceControllerIntegrationTest {
         val name = "one"
         placeRepository.saveAll(listOf(placeOne.toPlace(), placeTwo.toPlace()))
         mockMvc.perform(get("$BASE_REQUEST_URI?name=$name"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfElements").value(1))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfElements").value(1))
     }
 
     // Delete place
@@ -174,16 +177,16 @@ class PlaceControllerIntegrationTest {
     fun `should be able to delete a place and return status code 204`() {
         val createdPlace = placeRepository.save(placeRequestDTO.toPlace())
         mockMvc.perform(delete("$BASE_REQUEST_URI/${createdPlace.id}"))
-            .andExpect(MockMvcResultMatchers.status().isNoContent)
+                .andExpect(MockMvcResultMatchers.status().isNoContent)
     }
 
     @Test
     fun `should not be able to delete a place with nonexistent id and return status code 404`() {
         val placeId = UUID.randomUUID()
         val response = mockMvc.perform(delete("$BASE_REQUEST_URI/$placeId"))
-            .andExpect(MockMvcResultMatchers.status().isNotFound)
-            .andReturn()
-        assertThat(response.response.contentAsString).isEqualTo("Place with id $placeId not found")
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+                .andReturn()
+        assertThat(response.response.contentAsString).isEqualTo("Place with id $placeId not found!")
     }
 
 
@@ -193,9 +196,9 @@ class PlaceControllerIntegrationTest {
 
         @Container
         @JvmStatic
-        val container = PostgreSQLContainer("postgres:latest")
-            .withUsername("postgres")
-            .withPassword("postgres")
+        val container: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14")
+                .withUsername("postgres")
+                .withPassword("postgres")
 
         @DynamicPropertySource
         @JvmStatic
